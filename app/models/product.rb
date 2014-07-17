f3ccb05ae88f3815bacf0deeb3630286591ec295
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-07-17 20:59:26 +0600

    Added Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true#, length: { minimum: 10 }
@@ -11,5 +15,17 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+
+    # Убеждаемся в отсутсвии товарных позиций, ссылающихся на данный товар
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'существуют связанные товарные позиции в корзине покупателя')
+        return false
+      end
+    end
 
 end
